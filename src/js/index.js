870d8d3bc4c5e4878c5c658b9828d70b57a573ff
Richard Chotvac <chotvacr@gmail.com>
2020-05-22 11:43:32 +0200

    move right

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,43 @@
 
 console.log('it works!');
 
-let mouth = 'open';
-const PacOpen = document.querySelector('.pacboy-active-light') ;
+// let mouth = 'open';
+// const PacOpen = document.querySelector('.pacboy-active-light') ;
 
-document.addEventListener('click', () => {
-    if (mouth === 'open') {
-        PacOpen.style.backgroundPositionX = '85px';
-        mouth = 'closed';
-    }
-    else  {
-        PacOpen.style.backgroundPositionX = '0px';
-        mouth='open';
-    }
-        });
-   
-  
+// document.addEventListener('click', () => {
+//     if (mouth === 'open') {
+//         PacOpen.style.backgroundPositionX = '85px';
+//         mouth = 'closed';
+//     }
+//     else  {
+//         PacOpen.style.backgroundPositionX = '0px';
+//         mouth='open';
+//     }
+//         });
+
+ 
+ 
+
+const move = document.querySelector('.entity--pac');
+const PacOpen = document.querySelector('.pacboy-active-light');
+
+let TILE_SIZE = 85; 
+let xpos=1; 
+let mouth = 'open';
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'ArrowRight') {
+                move.style.transform = `translateX(${xpos*TILE_SIZE}px)`;
+                xpos +=1;
+
+            if (mouth === 'open') {
+                PacOpen.style.backgroundPositionX = '85px';
+                mouth = 'closed';
+             }
+            else  {
+                PacOpen.style.backgroundPositionX = '0px';
+                mouth='open';
+             }
+            }
+            
+        }
+        );
