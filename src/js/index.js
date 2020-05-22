e70d7596d7b1b90c1b700337c94084dc5d0c2705
Richard Chotvac <chotvacr@gmail.com>
2020-05-22 10:50:21 +0200

    first stage

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('it works!');
+
+let mouth = 'open';
+const PacOpen = document.querySelector('.pacboy-active-light') ;
+
+document.addEventListener('click', () => {
+    if (mouth === 'open') {
+        PacOpen.style.backgroundPositionX = '85px';
+        mouth = 'closed';
+    }
+    else  {
+        PacOpen.style.backgroundPositionX = '0px';
+        mouth='open';
+    }
+        });
+   
+  
